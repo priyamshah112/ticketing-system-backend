260f57f564346be3206e40e555149973dd1e707c
Zeem Zach <zeemzach@gmail.com>
2022-03-07 23:28:50 +0530

    cors issue

--- config/cors.php
+++ config/cors.php
@@ -18,7 +18,7 @@ return [
     'paths' => ['api/*', 'api/admin/*', 'api/users/*', '*'],
     'allowed_methods' => ['POST', 'GET', `DELETE`, 'PUT', '*'],
     'allowed_origins' => ['http://localhost:3000', 'https://client.myapp.com', 'http://142.93.211.147:3000', 'http://142.93.211.147:8080', 'http://142.93.211.147:80', '*'],
-    'allowed_headers' => ['Origin', 'Content-Type', 'Accept', 'X-Custom-Header', 'X-Auth-Token', 'Upgrade-Insecure-Requests', '*'],
+    'allowed_headers' => ['Origin', 'Content-Type', 'Authorization', 'Accept', 'X-Custom-Header', 'X-Auth-Token', 'Upgrade-Insecure-Requests', '*'],
     'exposed_headers' => ['*'],
     'max_age' => 0,
     'supports_credentials' => false,
