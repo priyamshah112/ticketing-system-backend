591a1f8b448e081bdc8c1e9452b0cbbefb6feade
Mustafa Bharmal <50296000+Mustafa535152@users.noreply.github.com>
2022-04-07 12:11:14 +0530

    Country API Done

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -10,4 +10,16 @@ class Country extends Model
     use HasFactory;
 
     protected $guarded = ['id'];
+
+    public function ticket()
+    {
+        return $this->hasManyThrough(
+            Ticket::class,
+            User::class,
+            'country_id', // Foreign key on users table...
+            'created_by', // Foreign key on posts table...
+            'id', // Local key on countries table...
+            'id' // Local key on users table...
+        );
+    }
 }
